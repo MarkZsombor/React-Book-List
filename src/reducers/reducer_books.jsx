d0ff82c9afc950cc07a7b37423af40660d8f221a
Mark Zsombor <markszsombor@gmail.com>
2018-04-02 15:31:28 -0700

    app renders list of books defined by redux

---
+++ src/reducers/reducer_books.jsx
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Gun, Germs, and Steel' },
+    { title: 'Assassin\'s Apprentice' },
+    { title: 'Black Swan' },
+    { title: 'Steelheart' },
+  ];
+}
