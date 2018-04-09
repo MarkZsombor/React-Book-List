f4c489ead34780f346113da10ce06ce2b6261ce6
Mark Zsombor <markszsombor@gmail.com>
2018-04-08 21:47:03 -0700

    fixed switch statement in reducer

--- src/reducers/reducer_active_book.jsx
+++ src/reducers/reducer_active_book.jsx
@@ -1,8 +1,9 @@
 // State argument is the state of the reducer, not App state
 export default function (state = null, action) {
-  switch(action.type) {
+  switch (action.type) {
     case 'BOOK_SELECTED':
       return action.payload;
-  };
-  return state;
+    default:
+      return state;
+  }
 }
